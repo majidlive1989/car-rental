e923ac2a4b69f9b6f84d3f89da7325641633fb6b
m.ali <majid.live@gmail.com>
2025-11-07 17:36:03 +0330

    v1

---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+// import { create } from "zustand";
+
+// type offcanvasT = {
+//   open: boolean;
+//   setOpen: (newState: boolean) => void;
+//   // Toggle: () => void;
+//   // close: () => void;
+// };
+
+// export const useOffcanvas = create<offcanvasT>()((set) => ({
+//   open: false,
+//   setOpen: (newState) => set(() => ({ open: newState })),
+//   // Toggle: () => set((previous) => ({ open: !previous.open })),
+//   // close: () => set(() => ({ open: false })),
+// }));
